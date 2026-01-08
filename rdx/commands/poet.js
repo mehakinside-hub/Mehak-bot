8bf7793a62e032b536736a7038f978203de123da
mehakinside-hub <gulaabinside@gmail.com>
2026-01-09 02:50:50 +0500

    Update poet.js

--- rdx/commands/poet.js
+++ rdx/commands/poet.js
@@ -49,7 +49,7 @@ module.exports = {
       const selectedLine = poetryLines[Math.floor(Math.random() * poetryLines.length)];
 
       const message = {
-        body: `꧁🍒❤️‍🔥${name}❤️‍🔥🍒꧂\n\n『꧁🍒\n   ${selectedLine} 🍒꧂』\n\n❤️𝕆𝕎ℕ𝔼ℝ : ꧁𝐙𝐚𝐢𝐧𝐢-𝐉𝐮𝐭𝐭꧂🌹`
+        body: `꧁🍒❤️‍🔥${name}❤️‍🔥🍒꧂\n\n『꧁🍒\n   ${selectedLine} 🍒꧂』\n\n❤️𝕆𝕎ℕ𝔼ℝ : ꧁*دی لـیجـۧۛنڈ مـۧۛـہـۧۛـک جٹـۧۛـی انـسـۧۛـائـیـڈ💀💸*꧂🌹`
       };
 
       return api.sendMessage(message, threadID, messageID);
@@ -84,6 +84,6 @@ module.exports = {
 
     const selectedLine = poetryLines[Math.floor(Math.random() * poetryLines.length)];
 
-    return send.reply(`꧁🍒❤️‍🔥${name}❤️‍🔥🍒꧂\n\n『꧁🍒\n   ${selectedLine} 🍒꧂』\n\n❤️𝕆𝕎ℕ𝔼ℝ : ꧁𝐙𝐚𝐢𝐧𝐢-𝐉𝐮𝐭𝐭꧂🌹`);
+    return send.reply(`꧁🍒❤️‍🔥${name}❤️‍🔥🍒꧂\n\n『꧁🍒\n   ${selectedLine} 🍒꧂』\n\n❤️𝕆𝕎ℕ𝔼ℝ : ꧁*دی لـیجـۧۛنڈ مـۧۛـہـۧۛـک جٹـۧۛـی انـسـۧۛـائـیـڈ💀💸*꧂🌹`);
   }
 };
